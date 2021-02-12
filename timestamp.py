128417f9765a77f2cc9910cee8ff37c02698a5ef
monkeyman192 <monkey_man_192@yahoo.com.au>
2021-02-12 17:34:23 +1100

    added the time frames and more

--- timestamp.py
+++ timestamp.py
@@ -12,6 +12,12 @@ REPORT_NESTED_DIFF = (
     "Average time between '{0}' and '{1}' inside function '{2}' over {3} "
     "(={4}) repetitions: {5}s")
 
+REPORT_TIMEFRAME = (
+    "Function '{0}':\n"
+    "\tTime run: {1}\n"
+    "\tAverage run time: {2}\n"
+)
+
 
 class NoStampException(Exception):
     pass
@@ -21,11 +27,70 @@ class InvalidStampException(Exception):
     pass
 
 
+# TODO: Have an formatted and non-formatted version?
+def calc_averages(start_times: list, end_times: list, start_desc: str = None,
+                  end_desc: str = None, func_name: str = None) -> str:
+    avgs = []
+    if len(start_times) == len(end_times):
+        # Return the number of times run and the average amount of time
+        # per run.
+        avgs = [end_times[i] - start_times[i]
+                for i in range(len(start_times))]
+        if func_name:
+            if len(avgs) > 1:
+                return REPORT_FUNC_AVG.format(
+                    func_name, len(avgs), sum(avgs) / len(avgs))
+            elif len(avgs) == 1:
+                return REPORT_FUNC_VAL.format(func_name, avgs[0])
+        else:
+            if len(avgs) > 1:
+                return REPORT_DIFF_AVG.format(
+                    start_desc, end_desc, len(avgs), sum(avgs) / len(avgs))
+            elif len(avgs) == 1:
+                return REPORT_DIFF_VAL.format(start_desc, end_desc, avgs[0])
+    return avgs
+
+
 class FunctionTimeFrame():
     """ An object to contain all the information about a particular set of
     timestamps within a function."""
+    __slots__ = ('name', 'time_stamps')
+
     def __init__(self, func_name):
         self.name = func_name
+        self.time_stamps = {}
+
+    def stamp(self, key: Union[str, int]):
+        _curr_time = time.time()
+        if key not in self.time_stamps:
+            self.time_stamps[key] = [_curr_time]
+        else:
+            self.time_stamps[key].append(_curr_time)
+
+    @property
+    def times_run(self) -> int:
+        return len(self.time_stamps.get(-1, []))
+
+    @property
+    def average_runtime(self) -> float:
+        start_times = self.time_stamps.get(0, [])
+        end_times = self.time_stamps.get(-1, [])
+        return (sum(end_times) - sum(start_times)) / self.times_run
+
+    def runtime(self) -> str:
+        start_times = self.time_stamps.get(0, [])
+        end_times = self.time_stamps.get(-1, [])
+        return calc_averages(start_times, end_times, func_name=self.name)
+
+    def report(self) -> str:
+        """ Return a nicely formatted report of the runtimes of this particular
+        function. """
+        return REPORT_TIMEFRAME.format(self.name,
+                                       self.times_run,
+                                       self.average_runtime)
+
+    def __str__(self) -> str:
+        return f"Time frame for {self.name} containing the timestamps {self.time_stamps}"
 
 
 class Stamp():
@@ -33,6 +98,9 @@ class Stamp():
 
     def __init__(self, name):
         self._name = name
+        self._time_stamps = {}
+        self._func_time_stamps = {}
+        # TODO: do we need the function stack??
         self._func_stack = []
         self._func_deltas = {}
         self._reported_functions = set()
@@ -78,14 +146,20 @@ class Stamp():
         else:
             self._func_stack.append(func_name)
         # Also add the start time of the function to the time stamps
-        self._stamp((func_name, 0))
+        func_tf = self._func_time_stamps.get(func_name,
+                                             FunctionTimeFrame(func_name))
+        func_tf.stamp(0)
+        if func_name not in self._func_time_stamps:
+            self._func_time_stamps[func_name] = func_tf
 
     def exit_function(self):
         """ Remove the most recent function from the stack. """
         if self._func_stack:
             func_name = self._func_stack.pop(-1)
         # Also add the start time of the function to the time stamps
-        self._stamp((func_name, -1))
+        func_tf = self._func_time_stamps.get(func_name,
+                                             FunctionTimeFrame(func_name))
+        func_tf.stamp(-1)
 
     def _stamp(self, key: Union[str, int]):
         _curr_time = time.time()
@@ -97,40 +171,18 @@ class Stamp():
     def stamp(self, desc: str):
         if not isinstance(desc, str):
             raise InvalidStampException("Description must be a string")
-        # TODO: fix this to allow multiplely nested functions...
+        # TODO: fix this to allow multiply nested functions...
         if self._func_stack:
-            key = (self._func_stack[-1], desc)
+            self._func_time_stamps[self._func_stack[-1]].stamp(desc)
         else:
-            key = desc
-        self._stamp(key)
+            self._stamp(desc)
 
-    def _calc_averages(self, start_times: list, end_times: list,
-                       start_desc: str = None, end_desc: str = None,
-                       func_name: str = None) -> str:
-        avgs = []
-        if len(start_times) == len(end_times):
-            # Return the number of times run and the average amount of time
-            # per run.
-            avgs = [end_times[i] - start_times[i]
-                    for i in range(len(start_times))]
-            if func_name:
-                if len(avgs) > 1:
-                    return REPORT_FUNC_AVG.format(
-                        func_name, len(avgs), sum(avgs) / len(avgs))
-                elif len(avgs) == 1:
-                    return REPORT_FUNC_VAL.format(func_name, avgs[0])
-            else:
-                if len(avgs) > 1:
-                    return REPORT_DIFF_AVG.format(
-                        start_desc, end_desc, len(avgs), sum(avgs) / len(avgs))
-                elif len(avgs) == 1:
-                    return REPORT_DIFF_VAL.format(start_desc, end_desc, avgs[0])
-        return avgs
-
-    def func_runtime(self, func_name: str):
-        start_times = self._time_stamps.get((func_name, 0), [])
-        end_times = self._time_stamps.get((func_name, -1), [])
-        return self._calc_averages(start_times, end_times, func_name=func_name)
+    def func_runtime(self, func_name: str) -> str:
+        tf = self._func_time_stamps.get(func_name)
+        if tf:
+            return tf.runtime()
+        else:
+            return 'No function exists with this name...'
 
     def difference(self, start: str, end: str, func_name: str = None) -> str:
         if func_name:
@@ -139,11 +191,12 @@ class Stamp():
         else:
             start_times = self._time_stamps.get(start, [])
             end_times = self._time_stamps.get(end, [])
-        return self._calc_averages(start_times, end_times, start, end)
+        return calc_averages(start_times, end_times, start, end)
 
     def report(self):
         """ Return a nicely formatted report of the running times. """
-        return self._reported_functions
+        for fname in self._reported_functions:
+            print(self._func_time_stamps[fname].report())
 
 
 def report(stamp_name=None, deltas=[]):
@@ -185,12 +238,18 @@ if __name__ == "__main__":
     def f(x):
         for _ in range(x):
             ts.stamp('start')
-            time.sleep(0.3)
+            time.sleep(0.1)
             ts.stamp('end')
+
+    ts.stamp('before function')
     for i in range(4):
         f(5)
+    ts.stamp('after function')
 
-    print(ts._time_stamps)
+    print(ts._func_time_stamps['f'])
+    print('-----------------')
     print(ts.func_runtime('f'))
-    print(ts.difference('start', 'end', 'f'))
+    print('-----------------')
+    print(ts.difference('before function', 'after function'))
+    print('-----------------')
     print(ts.report())
